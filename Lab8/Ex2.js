546ad25d9724005ae50c908e3d8aae61b72e4ade
jordanwc <jordanwc@hawaii.edu>
2021-03-01 22:19:33 -1000

    SmartPhoneProducts2

--- Lab8/Ex2.js
+++ Lab8/Ex2.js
@@ -1,9 +1,7 @@
-require("./product_data.js");
-
-var num_products =5;
-
-for (var item_num = 1; eval("typeof name"+item_num) != 'undefined'; item_num++ ){
-console.log(`${item_num}.  ${eval('name'+item_num)}`);
+require("./products_data.js");
+ 
+for(var item_num = 1; eval("typeof name"+item_num) != 'undefined'; item_num++ ){
+console.log (`${item_num}. ${eval('name'+item_num)}`);
 }
-
+ 
 console.log ("That's all we have!");
