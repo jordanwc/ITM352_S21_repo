051ac231c0468f9b7c4347436c553b9cf171620c
jordanwc <jordanwc@hawaii.edu>
2021-03-16 23:17:31 -1000

    DayOfTheWeek1

---
+++ Lab11/Ex5.js
@@ -0,0 +1,15 @@
+function junk1() {
+    for(let i1=1; i1<2; i1++){console.log(i1);}
+    console.log(`i1 is ${i1}`);
+}
+
+
+function junk2() {
+    for(var i2=1; i2<2; i2++){console.log(i2);}
+    console.log(`i2 is ${i2}`);
+}
+
+function junk3() {
+    for(i3=1; i3<=2; i3++){console.log(i3);}
+    console.log(`i3 is ${i3}`);
+}
--- Lab9/Ex1.js
+++ Lab9/Ex1.js
@@ -1,9 +1,10 @@
 m=11;
 d=28;
 
-repeat forever{ step 
-step
-if not step then
+repeat forever{ 
+step 
+if not step then {
  turn right
 
+    }
 }
