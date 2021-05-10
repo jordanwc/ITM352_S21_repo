03cd50e092270230a4d346687e1048eefb0ac756
jordanwc <jordanwc@hawaii.edu>
2021-05-09 23:22:16 -1000

    update

--- Chen_Jordan_Assignment2/server.js
+++ Chen_Jordan_Assignment2/server.js
@@ -1,68 +1,100 @@
-var express = require('express');
-var app = express();
-var myParser = require("body-parser");
-var data = require('./public/products_data.js');
-var products = data.products
-app.use(myParser.urlencoded({ extended: true }));
-var qs = require('qs');
-var fs = require('fs'); // Loads/ starts up fs system actions
-const querystring = require('querystring');
+//Author: Jordan Chen 
+//Assignment 2 file for my sever
+//Followed Professor Port's Assignment 2 screencast
 
-// detects request for all and displays path - code taken from Lab 13 Ex 4
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path);
-    next();
+var express = require('express'); // Loads express
+var app = express(); // Variable called app that starts the express module
+var myParser = require("body-parser");
+var data = require('./public/products_data'); //variable set to data and links to my public folder with products_data.js
+var products = data.products // Loads my products from products_data.js
+app.use(myParser.urlencoded({ extended: true }));
+var qs = require('qs'); // Loads starts up query string
+var fs = require('fs'); // Loads starts up fs system actions
+const querystring = require('querystring'); // Uses the query string 
+
+
+var user_data_file = "./user_data.json"; // Reads user_data.json, used code from Lab 14
+if (fs.existsSync(user_data_file)) { // Checks the data to see if the file is there
+    var file_stats = fs.statSync(user_data_file) // Gets the data from the files
+    var user_data = JSON.parse(fs.readFileSync(user_data_file, "utf-8"));
+}
+console.log(user_data); // Displays the request for user_data
+// detects request for all and displays path, code taken from Lab 13 
+app.all('*', function (request, response, next) { // Connects all my request for POST
+    console.log(request.method + ' to path ' + request.path); //Displays the request for method and path
+    next(); // Continue
 });
-// Taken from Lab 13
-app.post("/process_page", function (request, response, next) {
-    // following code same as code in products_display but modified
+
+// Borrowed code from Lab 13
+app.post("/process_page", function (request, response, next) { // Processes product form
     let POST = request.body;
-    console.log(POST);
-    const stringified = querystring.stringify(POST);
-    has_errors = false; // assume quantities are valid
-    total_qty = 0; 
-    for (i in products) {
-        if (typeof POST[`quantity${i}`] != "undefined") {
-            a_qty = POST[`quantity${i}`];
-            total_qty += a_qty; 
-            if(!isNonNegInt(a_qty)) {
-                has_errors = true; //invalid quantity error
+    console.log(POST); // Displays the products and quantity
+    const stringified = querystring.stringify(POST); // Stores quantity of products through query string
+    has_errors = false; // Assume quantities are valid
+    total_qty = 0; // Default for quantity of each product
+    for (i in products) { // For loop checking the quantity for each product
+        if (typeof POST[`quantity${i}`] != "undefined") { //
+            a_qty = POST[`quantity${i}`]; // The total quantity entered for each product
+            total_qty += a_qty; //Increase in quantity
+            if (!isNonNegInt(a_qty)) { // Checks for a non negative integer
+                has_errors = true; // Invalid quantity error
             }
         }
     }
-    // Respond to errors or redirect to login if no errors - response.redirect code found from Getting start with Assign. 2 screencast
-    if(has_errors) { // if has errors then keeps user on products_display page
-        response.redirect("./products_display.html?" + stringified);
-    } else if(total_qty == 0) { // if no quantity selection then keeps user on products_display page
-        response.redirect("./products_display.html?" + stringified);
-    } else { // if nothing wrong then send user to login page
-        response.redirect("./login.html?" + stringified);
+    // Respond to errors or redirect to login if no errors, used code from Professor Port's assignment 2 screencast
+    if (has_errors) { // if has errors then keeps user on products_display page
+        response.redirect("./products_display.html?" + stringified);  //Redirect user to the product_display page
+    } else if (total_qty == 0) { // if no quantity selection then keeps user on products_display page
+        response.redirect("./products_display.html?" + stringified); //Redirect user to the product_display page
+    } else {
+        response.redirect("./login.html?" + stringified);  // If nothing is wrong then send user to login page
     }
 })
 
-// Processes login form - part of code from Assignment 2 screencast
-app.post('/process_login', function (request, response, next) {
-    console.log(request.query);
-    request.query["uname"] = request.body["uname"];
-    response.redirect("invoice.html?" + qs.stringify(request.query)); 
-});
-
-// Processes register form - part of code from Assignment 2 screencast
-app.post('/process_register', function (request, response, next) {
-    console.log(request.query);
-    request.query["uname"] = request.body["uname"];
-    response.redirect("invoice.html?" + qs.stringify(request.query)); 
-});
-
-
-// function code from Invoice4
-function isNonNegInt(q, returnErrors = false) {
-    var errors = []; // assume that there are no errors
-    if(q == "") q = 0; // Considers blank inputs as 0
-    if(Number(q) != q) errors.push('Not a number!'); // Check if string is number
-    else if(q < 0) errors.push('Negative value!'); // Check if non-negative
-    else if(parseInt(q) != q) errors.push('Not an integer!'); // Check if integer
-    return returnErrors ? errors : (errors.length == 0);
+app.post('/process_login', function (request, response, next) { // Creating the function for the login page using POST
+    console.log(request.body); //Display the request for the body
+    let username_entered = request.body["Username"]; // Sets user input for username as Username
+    request.query.Username = username_entered; // Checks and request the username entered to see if it is in the database
+    let password_entered = request.body["psw"]; // Sets user input for psw as psw
+    // Checks if username_entered matches any usernames in user_data
+    if (typeof user_data[username_entered] != "undefined") { //Checks if the username is in the registered
+        if (user_data[username_entered]["password"] == password_entered) { // Checks if password matches the password that is connected to that username
+            response.redirect("invoice.html?" + qs.stringify(request.query)); // Sends user to invoice if no errors
+            return;
+        }
+    } else {
     }
-app.use(express.static('./public'));
-app.listen(8080, () => console.log(`listening on chen 8080`));
+    // Sends back to login if login info does not match user_data info
+    response.redirect("login.html?" + qs.stringify(request.query)); // Redirect user to the login page
+});
+
+// Processes register form, used code from Professor Port's Assignment 2 screencast
+app.post('/process_register', function (request, response, next) { // Creating the function for the register page using POST
+    console.log(request.query); // Display the request for the register page in the query
+    request.query["Username"] = request.body["Username"]; //Requesting for the textbox and label for the username from register.html
+    request.query["email"] = request.body["email"]; //Requesting for the textbox and label for the email from register.html
+    request.query["psw"] = request.body["psw"]; // Requesting for the textbox and label for the password from register.html
+    response.redirect("login.html?" + qs.stringify(request.query)); //When submitted redirects to the login page
+});
+
+// Processes login form, used code from Professor Port's Assignment 2 screencast
+app.post('/process_login', function (request, response, next) {
+    console.log(request.query); //Display the request for the login page in the query
+    request.query["Username"] = request.body["Username"]; //Requesting for the textbox and label for the username from login.html
+    request.query["email"] = request.body["email"]; //Requesting for the textbox and label for the email from login.html
+    request.query["psw"] = request.body["psw"]; //Requesting for the textbox and label for the password from login.html
+    response.redirect("invoice.html?" + qs.stringify(request.query)); //If successfully logged in user is sent to the invoice
+
+});
+
+// Used code from Invoice 4
+function isNonNegInt(q, returnErrors = false) {
+    var errors = []; // Assume that there are no errors
+    if (q == "") q = 0; // Considers blank inputs as 0
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is number
+    else if (q < 0) errors.push('Negative value!'); // Check if non-negative
+    else if (parseInt(q) != q) errors.push('Not an integer!'); // Check if integer
+    return returnErrors ? errors : (errors.length == 0); //Show errors if returned as an error
+}
+app.use(express.static('./public')); //Using express module from the public folder
+app.listen(8080, () => console.log(`listening on chen 8080`)); //Requesting and instructing to listen on 8080
